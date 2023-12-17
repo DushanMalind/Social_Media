4e7776d4e8636d9472d7d76ca6d3ddb4643bafc5
DushanMalind <dushanmalinda010@gmail.com>
2023-12-17 16:34:32 +0530

    Authlayout Sign up Image Add

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -10,9 +10,14 @@ const AuthLayout = () => {
                 <Navigate to={"/"}/>
             ):(
                 <>
-                    <section>
+                    <section className="flex flex-1 justify-center items-center flex-col py-10 ">
                         <Outlet/>
                     </section>
+
+                    <img src="../../public/assets/images/social5.jpg" alt="lgo"
+                         className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+
+                    />
                 </>
             )}
         </>
